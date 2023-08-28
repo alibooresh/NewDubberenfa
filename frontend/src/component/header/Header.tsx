9c0dd3afca1580dc1ae497c924ae8f519a42bcef
Ali Booresh <alibooresh1936@gmail.com>
2023-08-28 21:54:33 +0430

    Add Responsive Appbar

--- frontend/src/component/header/Header.tsx
+++ frontend/src/component/header/Header.tsx
@@ -1,25 +1,167 @@
-import { AppBar, Container } from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
+import MicIcon from "@mui/icons-material/Mic";
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
 
-export default function Header(){
-    return(
-        <AppBar position="static">
-            <Container>
-                <div>
-                    <ul>
-                        <li>
-                            <Link to={"/login"}>Login</Link>
-                        </li>
-                        <li>
-                            <Link to={"/signup"}>Signup</Link>
-                        </li>
-                        <li>
-                            <Link to={"/home"}>Home</Link>
-                        </li>
-                    </ul>
-                </div>
-            </Container>
-        </AppBar>
-    );
+export interface Page{
+    titlle:string,
+    href:string
+}
+const pages: Page[] = [
+  { titlle: "Start Project", href: "/project/create" },
+  { titlle: "Projects", href: "/project" },
+];
+const settings: Page[] = [
+  { titlle: "Profile", href: "/profile" },
+  { titlle: "Account", href: "/account" },
+  { titlle: "Dashboard", href: "/dashboard" },
+  { titlle: "Logout", href: "/login" },
+];
+
+export default function Header() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+  return (
+    <AppBar color="secondary" position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <MicIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            DubberEnFa
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.href} onClick={handleCloseNavMenu}>
+                  <Typography
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                      mr: 2,
+                      display: { xs: "none", md: "flex" },
+                      fontFamily: "monospace",
+                      fontWeight: 700,
+                      letterSpacing: ".1rem",
+                      color: "inherit",
+                      textDecoration: "none",
+                    }}
+                  >
+                    DubberEnFa
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                href={page.href}
+                key={page.href}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page.titlle}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting.href} onClick={handleCloseUserMenu}>
+                  <Button
+                    href={setting.href}
+                    key={setting.href}
+                    sx={{ color: "purple", display: "block" }}
+                  >
+                    {setting.titlle}
+                  </Button>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
